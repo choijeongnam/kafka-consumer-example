08055d11cee608ce8a4af0edd6002a0923a72130
DKSYSTEMS <DKSYSTEMS@NB-4001262>
2023-03-06 16:16:06 +0900

    testcontroller

--- src/main/java/com/lottecard/controller/TestController.java
+++ src/main/java/com/lottecard/controller/TestController.java
@@ -3,11 +3,17 @@ package com.lottecard.controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lottecard.dto.ResponseDTO;
+import com.lottecard.dto.TestDTO;
+
 @RestController
 public class TestController {
 
 	@PostMapping(value = "/test")
-	public Object test() {
-		return null;
+	public ResponseDTO test(TestDTO testDTO) {
+		ResponseDTO res = new ResponseDTO();
+		res.setCode(200);
+		res.setMessage("Sucess");
+		return res;
 	}
 }
