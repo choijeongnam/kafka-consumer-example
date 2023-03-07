e8a3a61198c728e774f26d9d5ab5a34d5e8e7382
HanSooKyeong <skhan.dongkuk@gmail.com>
2023-03-07 09:31:57 +0900

    오라클 디비 접속 추가 (기존 webpush)

--- src/main/java/com/lottecard/controller/TestController.java
+++ src/main/java/com/lottecard/controller/TestController.java
@@ -6,14 +6,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.lottecard.dto.ResponseDTO;
 import com.lottecard.dto.TestDTO;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+
 @RestController
 public class TestController {
 
 	@PostMapping(value = "/test")
+	@ApiImplicitParams({
+           @ApiImplicitParam(name = "name", required = true, dataType = "string", paramType = "path"),
+           @ApiImplicitParam(name = "age", required = true, dataType = "string", paramType = "query")
+   })
 	public ResponseDTO test(TestDTO testDTO) {
 		ResponseDTO res = new ResponseDTO();
-		res.setCode(200);
-		res.setMessage("Sucess");
+		//res.setCode(200);
+		//res.setMessage("Sucess");
 		return res;
 	}
 }
