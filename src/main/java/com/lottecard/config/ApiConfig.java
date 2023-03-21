bbfb8cc18ed46c0fd1263db5b39eea64c9a7c6ca
DKSYSTEMS <cjn4336@gmail.com>
2023-03-21 09:29:36 +0900

    okhttpclient 빈 추가

---
+++ src/main/java/com/lottecard/config/ApiConfig.java
@@ -0,0 +1,22 @@
+package com.lottecard.config;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import okhttp3.ConnectionPool;
+import okhttp3.OkHttpClient;
+
+@Configuration
+public class ApiConfig {
+	@Bean
+	private OkHttpClient getOkHttpClient() {
+		return new OkHttpClient()
+				.newBuilder()
+					.callTimeout(60, TimeUnit.SECONDS)
+					.connectionPool(new ConnectionPool())
+				.build();
+	}
+	
+}
