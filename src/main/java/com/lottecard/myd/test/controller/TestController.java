a963b32105f06d93f1f4e9ad8d343eb404494756
DKSYSTEMS <cjn4336@gmail.com>
2023-03-23 18:26:07 +0900

    cotroller 수정

--- src/main/java/com/lottecard/myd/test/controller/TestController.java
+++ src/main/java/com/lottecard/myd/test/controller/TestController.java
@@ -33,14 +33,14 @@ public class TestController {
 		//반면, HttpMessageNotReadableException은 요청 메시지를 읽을 때 발생할 수 있는 예외입니다. 예를 들어, 요청 바디가 JSON 형식이고, 해당 JSON의 필드에 필수 값이 누락된 경우 HttpMessageNotReadableException이 발생할 수 있습니다.
 		//따라서 MethodArgumentNotValidException이 발생하지 않고 HttpMessageNotReadableException이 발생하는 이유는 유효성 검증이 수행되기 전에 요청 메시지를 읽을 때 발생하는 예외로, 요청 바디의 형식이나 필드 값이 잘못된 경우 발생할 수 있습니다.
 		//유효성 검증과 요청 메시지 읽기는 스프링에서 다른 과정으로 처리되므로, 발생하는 예외도 서로 다를 수 있습니다. 따라서 HttpMessageNotReadableException이 발생하는 경우, 요청 바디의 형식이나 필드 값이 잘못되었는지 확인해야 합니다.
-		COMMONCodeVO vo = new COMMONCodeVO();
+		//COMMONCodeVO vo = new COMMONCodeVO();
 		ResponseDTO res = new ResponseDTO();
 		res.setCode(200);
 		//res.setMessage("Sucess");
 		//res.setMessage(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(cmon.selectList(vo)));
-		vo.setGroupCode("10000");
-		List<COMMONCodeVO> list = cmon.selectList(vo);
-		res.setMessage(list.toString());
+		//vo.setGroupCode("10000");
+		//List<COMMONCodeVO> list = cmon.selectList(vo);
+		res.setMessage("SUCCESS");
 		return res;
 	}
 
